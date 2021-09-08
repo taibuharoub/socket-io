207830ba06306d06dae0f25a56acc572fbd18605
Taibu Haroub <haroubtaibu@gmail.com>
2021-09-08 15:57:45 +0300

    Add namespace and romm classes

---
+++ classes/Namespace.js
@@ -0,0 +1,13 @@
+class Namespace {
+  constructor(id, nsTitle, img, endpoint) {
+    this.id = id;
+    this.nsTitle = nsTitle;
+    this.img = img;
+    this.endpoint = endpoint;
+    this.rooms = [];
+  }
+
+  addRoom(roomObj) {
+    this.rooms.push(roomObj);
+  }
+}
